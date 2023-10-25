59445850c4977f067b5670972acb9a71c6aa3df7
Namrata-Chand <namrata.chand@gmail.com>
2023-10-25 17:41:24 +0530

    changed the test branch version of HelloWorld.java

--- com.ripples.git.HelloWorldProj/src/com/ripples/git/HelloWorldProj/HelloWorld.java
+++ com.ripples.git.HelloWorldProj/src/com/ripples/git/HelloWorldProj/HelloWorld.java
@@ -3,7 +3,7 @@ package com.ripples.git.HelloWorldProj;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git World..changed...");
+		System.out.println("Hello Git World..changed the test branch..");
 	}
 
 }
